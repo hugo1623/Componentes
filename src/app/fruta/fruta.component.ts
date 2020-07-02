b988e57175046218a31fe7aaa321205d587668d6
Hugo Luis <hugoluis.system@gmail.com>
2020-07-01 21:59:43 -0500

    Ejercicio Componente Empleado

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'fruta',
-    templateUrl:'./fruta.component.html'
+    templateUrl:'./fruta.component.html',
 
 })
 export class FrutaComponent{
