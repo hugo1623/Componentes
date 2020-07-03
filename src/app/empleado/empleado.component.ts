c480c32a2042a6f7268578ef40b65a3ce173bc41
Hugo Luis <hugoluis.system@gmail.com>
2020-07-03 01:47:38 -0500

    Directiva *ngIf y evento Click

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Empleado } from './empleado';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+})
+export class EmpleadoComponent {
+
+  public titulo = 'Componente Empleado';
+  public empleado: Empleado;
+  public trabajadores: Array<Empleado>;
+  public trabajadorExterno:boolean;
+
+  constructor() {
+    this.empleado = new Empleado('Hugo Luis', 25, 'Desarrollador', true);
+    this.trabajadores = [
+      new Empleado('Ippo', 25, 'Desarrollador', true),
+      new Empleado('Kyra ', 24, 'Analista', false),
+      new Empleado('Dayron', 21, 'Diseñador', true),
+    ];
+    this.trabajadorExterno = true;
+  }
+
+  ngOnInit() {
+    console.log(this.empleado);
+    console.log(this.trabajadores);
+  }
+  cambiarExterno(valor){
+    this.trabajadorExterno = valor ;
+  }
+}
