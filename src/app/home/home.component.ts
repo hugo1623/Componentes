8732cf624a7548df1cc786067ec17b069262ec8b
Hugo Luis <hugoluis.system@gmail.com>
2020-07-04 17:39:38 -0500

    Ejercicio con Servicios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,24 @@ import { RopaService } from '../services/ropa.service';
 export class HomeComponent  {
 
   public titulo = "Página principal";
+  public listadoRopa:Array<string>;
+  public prendaGuardada:string;
   constructor(
     private _ropaService:RopaService,
   ){}
   ngOnInit(){
-    console.log(this._ropaService.prueba());
+    this.listadoRopa= this._ropaService.getRopa();
+    console.log(this.listadoRopa);
+  }
+  guardarPrenda(){
+    this._ropaService.addRopa(this.prendaGuardada)
+    this.prendaGuardada = null;
+
+  }
+  eliminarPrenda(index:number){
+    this._ropaService.deleteRopa(index);
+    alert(index);
+
   }
 
 
--- src/app/services/ropa.service.ts
+++ src/app/services/ropa.service.ts
@@ -2,8 +2,22 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class RopaService {
- public nombrePrenda='Pantalones xdxdxd';
+  public nombrePrenda = 'Pantalones';
+  public coleccionRopa = ['Pantalones Negros', 'Polos', 'Camisas'];
   prueba() {
     return this.nombrePrenda;
-   }
+  }
+  addRopa(nombrePrenda:string):Array<string> {
+    this.coleccionRopa.push(nombrePrenda);
+
+    return this.getRopa();
+  }
+  deleteRopa(index:number){
+    this.coleccionRopa.splice(index,1);
+    return this.getRopa();
+
+  }
+  getRopa():Array<string> {
+    return this.coleccionRopa;
+  }
 }
