acba9195508e35d48e44752d8f51e0045a03a17a
Hugo Luis <hugoluis.system@gmail.com>
2020-07-05 13:12:21 -0500

    Creación de servivios para hacer peticiones HTTP.

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
 import { Coche } from './coche';
+import { PeticionesService } from '../services/peticiones.service';
 
 @Component({
   selector: 'coches',
   templateUrl: './coches.component.html',
+  providers:[PeticionesService],
 })
 export class CochesComponent {
 
   public coche: Coche;
   public coches:Array<Coche>;
 
-  constructor(){
+  constructor(  
+    private _peticionesSerice: PeticionesService
+    ){
+  
     this.coche = new Coche('', '', '');
     this.coches = [
       new Coche('Tico', '1225','yelow'),
       new Coche('Nissan', '7725','black')
     ];
 
+  }
+  ngOnInit(){
+    console.log(this._peticionesSerice.getPrueba());
   }
 onSubmit(){
   this.coches.push(this.coche);
