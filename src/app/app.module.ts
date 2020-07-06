30884232d10dd01e880bae52f9c837f4a0ff4ed7
Hugo Luis <hugoluis.system@gmail.com>
2020-07-06 00:14:26 -0500

    Peticiones HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import{ routing, appRoutinProviders} from './app.routing'
+import{ routing, appRoutinProviders} from './app.routing';
 
 
 import { AppComponent } from './app.component';
 import { FrutaComponent} from './fruta/fruta.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+// import {HttpClient} from '@angular/common/http';
 
 import {HomeComponent} from './home/home.component';
 import {ContactoComponent}from './contacto/contacto.component';
 import { ConversorPipe } from './pipes/conversor.pipe';
 import { CochesComponent } from './coches/coches.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
